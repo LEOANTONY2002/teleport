1992d387dbce30fe9849bbe64d8465e49d33313c
LEO ANTONY <73683863+LEOANTONY2002@users.noreply.github.com>
2022-11-28 09:34:11 +0530

    Update Chat.js

--- client/src/screens/Chat.js
+++ client/src/screens/Chat.js
@@ -6,7 +6,7 @@ import { msgListAction, sendAction } from "../actions/chatAction";
 import "./Chat.css";
 import FileBase64 from "react-file-base64";
 
-const socket = io.connect("https://leoteleportsocket.herokuapp.com");
+const socket = io.connect("https://leoteleportsocket.onrender.com");
 
 function Chat() {
   const user = useSelector((state) => state.user);
